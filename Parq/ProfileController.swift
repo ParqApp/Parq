317e33079fde5307fde9f3c602d60c30882164b8
Elaina Hodgkin <ehodgkin9418@gmail.com>
2016-09-24 16:59:52 -0500

    create profile controller swift file and added to profile controller on storyboard

---
+++ Parq/ProfileController.swift
@@ -0,0 +1,31 @@
+//
+//  ProfileController.swift
+//  Parq
+//
+//  Created by Elaina Hodgkin on 9/24/16.
+//  Copyright © 2016 Carey Janecka. All rights reserved.
+//
+
+import UIKit
+
+
+class ProfileController: UIViewController {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width / 2;
+        self.profileImageView.clipsToBounds = true;
+        self.profileImageView.layer.borderWidth = 3.0;
+        self.profileImageView.layer.borderColor = UIColor.white.cgColor;
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
