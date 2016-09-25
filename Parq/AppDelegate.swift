01646d0775ba2bf758f8bbfa8b00e5126e31180f
ADEBOLA AKEREDOLU <adebolaakeredolu@ADEBOLAs-MacBook-Pro.local>
2016-09-25 13:29:41 -0500

    Changed default startup view and added Login

--- Parq/AppDelegate.swift
+++ Parq/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UIApplication.shared.setStatusBarStyle(UIStatusBarStyle.lightContent, animated: false)
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyCZRIFQqctXSarwoMNT6MUeMFwFACrO4dg")
         return true
---
+++ Parq/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Parq
+//
+//  Created by ADEBOLA AKEREDOLU on 9/25/16.
+//  Copyright © 2016 Carey Janecka. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
