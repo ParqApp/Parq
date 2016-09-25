a1776c927f4e5e3fb112bc36969cd058a6ca3765
Elaina Hodgkin <ehodgkin9418@gmail.com>
2016-09-24 20:03:01 -0500

    added tap function to maps controller

--- Parq/MapController.swift
+++ Parq/MapController.swift
@@ -10,6 +10,8 @@ import UIKit
 import GoogleMaps
 
 class MapController: UIViewController, GMSMapViewDelegate {
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +23,14 @@ class MapController: UIViewController, GMSMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView?.isMyLocationEnabled = true
+        mapView?.delegate = self
         view = mapView
         
         // Creates a marker in the center of the map.
@@ -37,9 +41,9 @@ class MapController: UIViewController, GMSMapViewDelegate {
         marker.map = mapView
     }
     
-    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
-        
-        print("You tapped at \(coordinate.latitude), \(coordinate.longitude)")
+    func mapView(_ mapView: GMSMapView!, didTap marker: GMSMarker!) -> Bool {
+        print("hello")
+        return true
     }
 
 
