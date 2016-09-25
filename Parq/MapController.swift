c5bd8a70811fcb69dd63a8956967494f2770f70a
Carey Janecka <figitaki96@yahoo.com>
2016-09-25 16:04:13 -0500

    Last minute updates.

--- Parq/MapController.swift
+++ Parq/MapController.swift
@@ -29,7 +29,7 @@ class MapController: UIViewController, GMSMapViewDelegate {
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 11.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView?.isMyLocationEnabled = true
         mapView?.delegate = self
