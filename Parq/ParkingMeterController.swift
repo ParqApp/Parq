8f67167dc7b2490df1ac9bcfd32de77645cad5ef
Elaina Hodgkin <ehodgkin9418@gmail.com>
2016-09-25 00:48:37 -0500

    hooked up objects

--- Parq/ParkingMeterController.swift
+++ Parq/ParkingMeterController.swift
@@ -13,6 +13,16 @@ class ParkingMeterController: UIViewController {
     
     @IBOutlet weak var parkingImage: UIImageView!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var rateLabel: UILabel!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var parkHereButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
